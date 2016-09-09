0b594378f56213c6ca037e425773da1c7580b08b
mhzed <minhongz@gmail.com>
2016-09-09 11:21:51 +0800

    Add os wrapper for networkInterfaces

--- rnio.js
+++ rnio.js
@@ -4,6 +4,7 @@ module.exports = {
   net : require("./lib/net"),
   //dgram : require("./lib/dgram"),
 
+  os : require("./lib/os"),
   // for testing
   tester : require("./lib/tester"),
   test : require("./test/fs.test"),
